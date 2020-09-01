8cb7207879320ea215d73f5fd97a15c105a146cb
siwel20 <plthrock@gmail.com>
2020-08-31 22:53:05 -0600

    inegrated front end for pizza-list and refactored

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,6 +2,7 @@ const {
     Schema,
     model
 } = require('mongoose');
+const moment = require('moment');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -13,6 +14,7 @@ const PizzaSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     size: {
         type: String,
@@ -26,6 +28,7 @@ const PizzaSchema = new Schema({
 }, {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 });
